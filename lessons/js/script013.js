a45d8db940ad68bf432b0e4343a170faa8bc1bed
zima <departmentservice17@gmail.com>
2023-12-07 17:27:37 +0200

    add less with theme about if-else-ternar-switch

---
+++ lessons/js/script013.js
@@ -0,0 +1,39 @@
+"use strict";
+//if-else
+
+// if(4 == 9){
+//   console.log('Ok!');
+// } else{
+//   console.log('Error!');
+// }
+
+// const num = 50;
+// if (num < 49){
+//   console.log('Error');
+// } else if (num > 100){
+//   console.log('Very more');
+// } else{
+//   console.log('Ok!');
+// }
+
+// ternar ?
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+
+// switch
+const num = 50;
+switch (num){
+  case 49:
+    console.log('Error!');
+    break;
+  case 100:
+    console.log('Error!');
+    break;
+  case 50:
+    console.log('ok!');
+    break;
+  default:
+    console.log('Not now!');
+    break;
+}
+
