14e1fe3eac466b68c4fd0e758e3369ae42c57b8a
zima <departmentservice17@gmail.com>
2023-12-07 17:53:19 +0200

    add less 14: cycle, break, continue

---
+++ lessons/js/script014.js
@@ -0,0 +1,25 @@
+// less 014
+
+"use strict";
+//while
+let num = 50;
+// while(num <= 55){
+//   console.log(num);
+//   num++;
+// }
+
+// do-while
+// do{
+//   console.log(num);
+//   num++;
+// }
+// while(num <= 55);
+
+// for -  break -  continue
+for(let i = 1; i < 8; i++){
+  if(i == 6){
+    //break;
+    continue;
+  }
+  console.log(i);
+}
