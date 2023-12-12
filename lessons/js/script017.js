6d0f3ee37f6f193597894219f2944e63312872b6
zima <departmentservice17@gmail.com>
2023-12-12 15:48:07 +0200

    add less 17

---
+++ lessons/js/script017.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// property and methods
+
+// text
+// const str = "TeSt";
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str);
+
+// const fruit = "some fruit";
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hello world";
+// console.log(logg.slice(6,11));
+// console.log(logg.slice(-5,-1));
+
+// console.log(logg.substring(6,11));
+
+// console.log(logg.substr(2,5));
+
+// number
+// const num = 12.2;
+// console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test))
+console.log(parseFloat(test))
